9f5ad94e9f9b272c021356b1352e3eb596406cbf
botondmajoross <botond.majoross@gmail.com>
2025-04-30 18:39:39 +0200

    ok gombból mentés lett a neve az edit formon

--- Coaches/Coaches/FormLogin.Designer.cs
+++ Coaches/Coaches/FormLogin.Designer.cs
@@ -42,27 +42,30 @@
             // 
             textBox1.Cursor = Cursors.IBeam;
             textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(165, 218);
+            textBox1.Location = new Point(189, 291);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(246, 29);
+            textBox1.Size = new Size(281, 34);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             textBox2.Cursor = Cursors.IBeam;
             textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(165, 285);
+            textBox2.Location = new Point(189, 380);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(246, 29);
+            textBox2.Size = new Size(281, 34);
             textBox2.TabIndex = 1;
             // 
             // button1
             // 
             button1.Cursor = Cursors.Hand;
             button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(165, 344);
+            button1.Location = new Point(189, 459);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(246, 37);
+            button1.Size = new Size(281, 49);
             button1.TabIndex = 2;
             button1.Text = "Belépés";
             button1.UseVisualStyleBackColor = true;
@@ -74,9 +77,9 @@
             label1.BackColor = SystemColors.ActiveCaptionText;
             label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.ButtonFace;
-            label1.Location = new Point(165, 194);
+            label1.Location = new Point(189, 259);
             label1.Name = "label1";
-            label1.Size = new Size(119, 21);
+            label1.Size = new Size(150, 28);
             label1.TabIndex = 3;
             label1.Text = "Felhasználónév";
             // 
@@ -86,9 +89,9 @@
             label2.BackColor = SystemColors.ActiveCaptionText;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = SystemColors.ButtonFace;
-            label2.Location = new Point(165, 261);
+            label2.Location = new Point(189, 348);
             label2.Name = "label2";
-            label2.Size = new Size(53, 21);
+            label2.Size = new Size(66, 28);
             label2.TabIndex = 4;
             label2.Text = "Jelszó";
             // 
@@ -99,9 +102,9 @@
             label4.BackColor = SystemColors.ActiveCaptionText;
             label4.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold, GraphicsUnit.Point);
             label4.ForeColor = SystemColors.ButtonFace;
-            label4.Location = new Point(177, 40);
+            label4.Location = new Point(202, 3);
             label4.Name = "label4";
-            label4.Size = new Size(234, 45);
+            label4.Size = new Size(286, 54);
             label4.TabIndex = 6;
             label4.Text = "ZSBKM Fitness";
             label4.TextAlign = ContentAlignment.MiddleCenter;
@@ -113,9 +116,9 @@
             label3.BackColor = SystemColors.ActiveCaptionText;
             label3.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold, GraphicsUnit.Point);
             label3.ForeColor = SystemColors.ButtonFace;
-            label3.Location = new Point(130, 85);
+            label3.Location = new Point(149, 63);
             label3.Name = "label3";
-            label3.Size = new Size(325, 45);
+            label3.Size = new Size(399, 54);
             label3.TabIndex = 7;
             label3.Text = "Bejelentkezési felület";
             label3.TextAlign = ContentAlignment.MiddleCenter;
@@ -126,19 +129,19 @@
             label5.BackColor = SystemColors.ButtonHighlight;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label5.ForeColor = Color.Red;
-            label5.Location = new Point(165, 146);
+            label5.Location = new Point(189, 195);
             label5.Name = "label5";
-            label5.Size = new Size(52, 21);
+            label5.Size = new Size(65, 28);
             label5.TabIndex = 8;
             label5.Text = "label5";
             label5.Visible = false;
             // 
             // FormLogin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.logo_blurred;
-            ClientSize = new Size(586, 487);
+            ClientSize = new Size(670, 548);
             Controls.Add(label5);
             Controls.Add(label3);
             Controls.Add(label4);
@@ -148,6 +151,7 @@
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FormLogin";
             Text = "FormLogin";
             Load += FormLogin_Load;
